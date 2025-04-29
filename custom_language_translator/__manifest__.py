bbf5ec39bddb1617e6bec898e37537f3fc330052
Ashraful-Reza <ashrafulreza.tanjil@gmail.com>
2025-04-29 16:05:16 +0600

    Added submodule: custom_language_translator

---
+++ custom_language_translator/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Custom Language Translator',
+    
+    'summary': "This module will help to Translate in Bengali Language (BD)",
+    'description': """This is  custom Translator .""",
+    
+    'author':"Metamorphosis Ltd, Tanjil",
+    'co-author':"Tanjil",
+    'website':'https://metamorphosis.com.bd',
+
+    'version': '18.0.0.1',
+    'category': 'Tools/Tools',
+    'license': 'AGPL-3',
+    
+    'depends': ['sale'],
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'wizard/sale_discount_approval_views.xml',
+    #     'views/sale_order_views.xml',
+    # ],
+
+    'sequence':-350,
+    "installable": True,
+    "application": True,
+    'auto_install': False,
+}
